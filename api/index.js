62905a79ebe1aa9f4bced45fd31c203bbdeab721
andre.matos <andre.matos@sisloc.com.br>
2023-10-20 21:31:07 -0300

    #01 - Inicialização do projeto, express, e Eslint

---
+++ api/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const bodyparser = require('body-parser')
+
+
+const app = express()
+
+app.use(bodyparser.json())
+
+const port = 3000
+
+app.get('/teste', (req, res) => {
+    res.status(200).send({ mensagem: 'Bem vindo a API' })
+})
+
+app.listen(port, () => {
+    return console.log(`listening on port ${port} http://localhost:${port}`)
+})
+
+module.exports = app
